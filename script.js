9ffe56d7d8c0ca6f65c1b7c83a4a21a44dbb603f
TerryHCollins <terry.hacheycollins@gmail.com>
2024-02-01 09:32:34 -0700

    added nav menu

---
+++ script.js
@@ -0,0 +1,10 @@
+const mobileMenuButton = document.getElementById("mobilemenu");
+
+const myLinks = document.getElementById("menuLinks");
+
+function toggleMobileMenu() {
+  // When menu icon clicked, toggle 'hidden' class on myLinks
+  myLinks.classList.toggle("hidden");
+}
+
+mobileMenuButton.addEventListener("click", toggleMobileMenu);
