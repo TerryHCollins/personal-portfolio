5783e255cb2fb5d11a731be6a3a24e7de29e9528
TerryHCollins <terry.hacheycollins@gmail.com>
2024-02-07 15:16:01 -0700

    commented out top contact icons//button for cleaner layout, traded form to formspree for tracking info

--- script.js
+++ script.js
@@ -6,7 +6,3 @@ function toggleMobileMenu() {
   // When menu icon clicked, toggle 'hidden' class on myLinks
   myLinks.classList.toggle("hidden");
 }
-
-// Add buttons
-//Contact Button
-//Form Submit Button
