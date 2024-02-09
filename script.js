6e11cc83ca03ed5d3460e3538a5c2de4070cd15b
TerryHCollins <terry.hacheycollins@gmail.com>
2024-02-09 13:01:43 -0700

    added MQ, added lang icons

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const mobileMenuButton = document.getElementById("mobilemenu");
 
 const myLinks = document.getElementById("menuLinks");
-
+// Says what to target for future toggle action
 function toggleMobileMenu() {
   // When menu icon clicked, toggle 'hidden' class on myLinks
   myLinks.classList.toggle("hidden");
 }
+// tells the mobile button to toggle on click interaction
+mobileMenuButton.addEventListener("click", toggleMobileMenu);
